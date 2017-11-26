6043bc128aaec9d7d1f36f2793e59a82c2011334
Franco Arolfo <francoarolfo@hotmail.com>
2017-11-26 18:53:24 -0300

    fix

--- truffle.js
+++ truffle.js
@@ -1,9 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "lURhQbmr92ZWN9vCjt55";
+var mnemonic = "repeat loan salad quantum toe walk peanut tiger fork arena piano van";
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3,
+      gas: 4212288
     }
   }
 };
